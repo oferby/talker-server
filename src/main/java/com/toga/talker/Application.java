ca5de07a1f722a9bfca36f2e562fc12bffb9ae65
oferb <ofer.benyacov@gmail.com>
2021-05-19 13:45:04 +0300

    first commit

---
+++ src/main/java/com/toga/talker/Application.java
@@ -0,0 +1,18 @@
+package com.toga.talker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] argv){
+
+        SpringApplication.run(Application.class, argv);
+
+    }
+
+}
