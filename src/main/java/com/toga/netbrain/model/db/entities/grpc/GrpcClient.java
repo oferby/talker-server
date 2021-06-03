5e67967a18ab52e55d9427d68ad035b751fab885
oferb <ofer.benyacov@gmail.com>
2021-06-03 16:52:21 +0300

    added gRPC

--- src/main/java/com/toga/netbrain/model/db/entities/grpc/GrpcClient.java
+++ src/main/java/com/toga/netbrain/model/db/entities/grpc/GrpcClient.java
@@ -1,12 +1,104 @@
 package com.toga.netbrain.model.db.entities.grpc;
 
+import com.toga.netbrain.service.*;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 
-import java.util.List;
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class GrpcClient {
 
+    private final Logger logger = LoggerFactory.getLogger(GrpcClient.class);
+
+    private Map<String, AgentServiceGrpc.AgentServiceBlockingStub> stubMap = new HashMap<>();
+    private Map<String, String> agentTargetMap = new HashMap<>();
+
+//    private AgentServiceGrpc.AgentServiceStub agentServiceStub;
+
+    public AgentHostInfoResponse requestAgentHostInfo(String agent) {
+
+        AgentServiceGrpc.AgentServiceBlockingStub blockingStub = this.getBlockingStub(agent);
+
+        AgentHostInfoRequest request = AgentHostInfoRequest.newBuilder().setHostId(agent).build();
+
+        AgentHostInfoResponse hostInfo = blockingStub.getHostInfo(request);
+
+        logger.debug("hostInfo: " + hostInfo);
+
+        return hostInfo;
+
+    }
+
+    public CreateAgentResponse createAgent(String agent, String target, String username, String password) {
+
+        agentTargetMap.put(target, agent);
+
+        AgentServiceGrpc.AgentServiceBlockingStub blockingStub = this.getBlockingStub(agent);
+
+        CreateAgentRequest createAgentRequest = CreateAgentRequest.newBuilder()
+                .setHost(target)
+                .setUsername(username)
+                .setPassword(password)
+                .build();
+
+        CreateAgentResponse response = blockingStub.createAgent(createAgentRequest);
+
+        logger.debug(response.toString());
+
+        return response;
+    }
+
+    public NodeDiscoveryResponse nodeDiscovery(String target) {
+
+        String agent = agentTargetMap.get(target);
+        AgentServiceGrpc.AgentServiceBlockingStub blockingStub = this.getBlockingStub(agent);
+
+        NodeDiscoveryRequest nodeDiscoveryRequest = NodeDiscoveryRequest.newBuilder()
+                .setHost(target).build();
+
+        NodeDiscoveryResponse response = blockingStub.runDiscovery(nodeDiscoveryRequest);
+
+        logger.debug(response.toString());
+
+        return response;
+    }
+
+
+    private AgentServiceGrpc.AgentServiceBlockingStub getBlockingStub(String agent) {
+
+        AgentServiceGrpc.AgentServiceBlockingStub blockingStub;
+
+        if (stubMap.containsKey(agent)) {
+            blockingStub = stubMap.get(agent);
+
+        } else {
+
+            ManagedChannelBuilder<?> channelBuilder =
+                    ManagedChannelBuilder.forAddress(agent, 51051).usePlaintext();
+            ManagedChannel channel = channelBuilder.build();
+
+            blockingStub = AgentServiceGrpc.newBlockingStub(channel);
+
+            stubMap.put(agent, blockingStub);
+
+        }
+
+        return blockingStub;
+
+    }
+
+    @PostConstruct
+    private void setup() {
+
+//        agentServiceStub = AgentServiceGrpc.newStub(channel);
+
+    }
 
 
 }
--- src/test/java/com/toga/netbrain/TestGrpc.java
+++ src/test/java/com/toga/netbrain/TestGrpc.java
@@ -1,68 +1,51 @@
 package com.toga.netbrain;
 
+import com.toga.netbrain.model.db.entities.grpc.GrpcClient;
 import com.toga.netbrain.service.*;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.annotation.PostConstruct;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestGrpc {
 
-    private AgentServiceGrpc.AgentServiceBlockingStub blockingStub;
-    private AgentServiceGrpc.AgentServiceStub agentServiceStub;
-
-    @PostConstruct
-    private void setup() {
-        ManagedChannelBuilder<?> channelBuilder = ManagedChannelBuilder.forAddress("localhost", 51051).usePlaintext();
-        ManagedChannel channel = channelBuilder.build();
-
-        blockingStub = AgentServiceGrpc.newBlockingStub(channel);
-        agentServiceStub = AgentServiceGrpc.newStub(channel);
-
-    }
+    @Autowired
+    private GrpcClient grpcClient;
 
     @Test
     public void testAgentHostInfo() {
 
+        String host = "localhost";
+        AgentHostInfoResponse hostInfo =
+                grpcClient.requestAgentHostInfo(host);
 
-        AgentHostInfoRequest request = AgentHostInfoRequest.newBuilder().setHostId("1").build();
-
-        AgentHostInfoResponse hostInfo = blockingStub.getHostInfo(request);
-
-        assert hostInfo != null;
-        System.out.println("Agent Hostname: " + hostInfo.getHostName());
-        System.out.println("Agents: " + hostInfo.getNumOfAgents());
-
-        CreateAgentRequest createAgentRequest = CreateAgentRequest.newBuilder()
-                .setHost("localhost")
-                .setUsername("oferb")
-                .setPassword("oferb")
-                .build();
-
-        CreateAgentResponse createAgentResponse = blockingStub.createAgent(createAgentRequest);
-
-        assert createAgentResponse.getAck();
-        System.out.println("agent created!");
-
-        hostInfo = blockingStub.getHostInfo(request);
-        System.out.println("Agents: " + hostInfo.getNumOfAgents());
+        assert hostInfo.getHostId().equals(host);
 
     }
 
     @Test
-    public void nodeDiscovery() {
+    public void testAddAgent() {
 
         String host = "localhost";
-        NodeDiscoveryRequest nodeDiscoveryRequest = NodeDiscoveryRequest.newBuilder()
-                .setHost(host).build();
+        String username = "oferb";
+        String password = "oferb";
 
-        NodeDiscoveryResponse response = blockingStub.runDiscovery(nodeDiscoveryRequest);
+        CreateAgentResponse createAgentResponse = grpcClient.createAgent(host, host, username, password);
+
+        assert createAgentResponse.getAck();
+        System.out.println("agent created!");
+
+//    }
+
+//    @Test
+//    public void nodeDiscovery() {
+
+//        String host = "localhost";
+
+        NodeDiscoveryResponse response = grpcClient.nodeDiscovery(host);
 
         assert response.getHost().equals(host);
 
